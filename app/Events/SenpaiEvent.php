1127fbb1ec7c164facae9e85fb72e5cc5e52e3ad
Damenjo Sitepu <damenjos@thebigrichgroup.com>
2023-07-15 17:33:25 +0700

    Setup: Websocket And First Implementation Using Public Channel

---
+++ app/Events/SenpaiEvent.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class SenpaiEvent implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    /**
+     * Create a new event instance.
+     */
+    public function __construct()
+    {
+        echo "I Feeling Lonely";
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return array<int, \Illuminate\Broadcasting\Channel>
+     */
+    public function broadcastOn(): array
+    {
+        return [
+            new Channel('public.senpai.1'),
+        ];
+    }
+
+    /**
+     * Custom Broadcast
+     *
+     * @return void
+     */
+    public function broadcastAs() {
+        return "senpai-ah";
+    }
+}
